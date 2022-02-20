e9507adbc47fecd3b6a1d0a3ee0f531165248ae2
Tanguy <tanguygous@gmail.com>
2022-02-20 20:21:55 +0100

    seconde commit to cocktails

--- src/app/cocktails-list/cocktails-list.component.ts
+++ src/app/cocktails-list/cocktails-list.component.ts
@@ -9,6 +9,12 @@ import { Cocktails } from '../interface/cocktails-interface';
 export class CocktailsListComponent implements OnInit {
   cocktails: Cocktails[] = [
     {
+      name: 'Mojito',
+      img: 'https://boiremixologie.com/files/medias/_thumb/mojito.jpg',
+      description:
+        'La légende met en vedette un pirate anglais qui aurait inventé cette recette lorsqu’il était à la conquête des mers de l’Amérique latine, dont Cuba.',
+    },
+    {
       name: 'Martini',
       img: 'https://boiremixologie.com/files/medias/_thumb/capture-ecran-martini.jpg',
       description:
@@ -31,12 +37,6 @@ export class CocktailsListComponent implements OnInit {
       img: 'https://boiremixologie.com/files/medias/_thumb/sirop_tonic_cola.jpg',
       description:
         'Le rhum &Coke, ou Cuba Libre pour le reste de la planète (au Québec, on aime être différent), a été popularisé à l’époque de la prohibition par les mafiosos qui profitaient de Cuba pour vendre leur alcool',
-    },
-    {
-      name: 'Mojito',
-      img: 'https://boiremixologie.com/files/medias/_thumb/mojito.jpg',
-      description:
-        'La légende met en vedette un pirate anglais qui aurait inventé cette recette lorsqu’il était à la conquête des mers de l’Amérique latine, dont Cuba.',
     },
   ];
 
